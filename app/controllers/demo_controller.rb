5ff2564fda6192d2a183fc3e635378aa04c1755c
SadekAlshaar018 <Your Actual Email, Sadek>
2018-02-02 23:27:00 +0100

    ruby on rails
    Ask Phill

---
+++ app/controllers/demo_controller.rb
@@ -0,0 +1,19 @@
+class DemoController < ApplicationController
+
+  layout false
+  def index
+    render ('index')
+  end
+
+  def hello
+    @array = [1,2,3,4,5,6]
+    render ('hello')
+  end
+  def other_hello
+    render ('otherhello')
+    # redirect_to(:action => 'otherhello')
+  end
+  def sadek
+    redirect_to('https://sadekalshaar.herokuapp.com/')
+  end
+end
