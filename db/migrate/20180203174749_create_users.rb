aaad6c78bb807775b40e968fd252e7b848859d3b
SadekAlshaar018 <Your Actual Email, Sadek>
2018-02-03 21:15:19 +0100

    Database
    Migrate and update

--- db/migrate/20180203174749_create_users.rb
+++ db/migrate/20180203174749_create_users.rb
@@ -1,8 +1,17 @@
 class CreateUsers < ActiveRecord::Migration[5.1]
   def change
     create_table :users do |t|
-
+      t.column "first_name", :string, :limit =>25
+      t.string "last_name", :limit => 50
+      t.string "email", :default => '', :null => false
+      t.string "password", :limit => 40
+      # t.datetime "created_at"
+      # t.datetime "updated_at"
       t.timestamps
     end
+  end
+  # after creat amodel we create a droptable
+  def down
+    drop_table  :users
   end
 end
